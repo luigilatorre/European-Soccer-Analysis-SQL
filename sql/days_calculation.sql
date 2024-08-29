3c4b93a980281b98f55956f2cf2a6618dfb5cea2
luigilatorre <164922404+luigilatorre@users.noreply.github.com>
2024-08-29 18:58:15 +0200

    Update days_calculation.sql

--- sql/days_calculation.sql
+++ sql/days_calculation.sql
@@ -1,11 +1,2 @@
-SELECT m.season,l.name,
-    COUNT(m.home_team_goal) as tot_goals,
-    MIN(m.home_team_goal) as min_goals,
-    MAX(m.home_team_goal) as max_goals,
-    AVG(m.home_team_goal) as avg_goals,
-    (MAX(m.home_team_goal) - MIN(m.home_team_goal))/2 as mid_range_goals,
-    SUM(m.home_team_goal) as sum_goals
-FROM sql-sandbox-418719.Final_Exercise.match m
-JOIN sql-sandbox-418719.Final_Exercise.leagues l ON m.league_id = l.id
-GROUP BY m.season,l.name
-ORDER BY season asc
+SELECT DATE_DIFF(MAX(date), MIN(date), DAY) AS DaysBetween
+  	FROM sql-sandbox-418719.Final_Exercise.match
