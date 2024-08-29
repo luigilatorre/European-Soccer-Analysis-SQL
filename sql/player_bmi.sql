2884944318d4b8e441db26278f34a3fd891fddf9
Luigi La Torre <164922404+luigilatorre@users.noreply.github.com>
2024-08-29 19:48:07 +0200

    Update player_bmi.sql

--- sql/player_bmi.sql
+++ sql/player_bmi.sql
@@ -1,15 +1,17 @@
 CREATE TABLE sql-sandbox-418719.Final_Exercise.PlayerBMI AS
 SELECT
-   				 id,
-  				 player_api_id,
-   				 player_name,
-   				 birthday,
-  				 weight / 2.205 AS kg_weight,
-    				 height / 100 AS m_height,
-    				(weight / 2.205) / ((height / 100) * (height / 100)) AS BMI
-FROM sql-sandbox-418719.Final_Exercise.player
+    id,
+    player_api_id,
+    player_name,
+    birthday,
+    weight / 2.205 AS kg_weight,
+    height / 100 AS m_height,
+    (weight / 2.205) / ((height / 100) * (height / 100)) AS BMI
+FROM
+    sql-sandbox-418719.Final_Exercise.player;
 
-SELECT * FROM sql-sandbox-418719.Final_Exercise.PlayerBMI
-WHERE BMI BETWEEN 18.5 AND 24.9
-
-
+SELECT *
+FROM
+    sql-sandbox-418719.Final_Exercise.PlayerBMI
+WHERE
+    BMI BETWEEN 18.5 AND 24.9;
